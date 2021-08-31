3acac96949851127c6386fc6f51f247bb050a8bf
hb1998 <rhabeeb1998@gmail.com>
2021-08-31 21:17:19 +0530

    fix: tried solid-js

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import type { Component } from "solid-js";
+import { Component, createSignal, For } from "solid-js";
 
-import logo from "./logo.svg";
 import styles from "./App.module.css";
 
+const listData =  new Array(1000).fill(null).map((_,index)=>({
+  name:`Label-${index}`,
+  height:30
+}))
+
 const App: Component = () => {
+  const [list, setList] = createSignal(listData);
+  const [window, setWindow] = createSignal([1,10]);
+  const calculatedHeight = listData.length * 30;
+
+  const [startIdx,endIdx] = window();
+  const renderedList = list().slice(startIdx,endIdx)
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div class={styles.App} >
+      <div style={{
+        "min-height":`${calculatedHeight}px`
+      }} className={styles.container}>
+      <For each={renderedList} fallback={<div>Loading...</div>}>
+      {(item) => <div>{item.name}</div>}
+      </For>
+      </div>
     </div>
   );
 };
