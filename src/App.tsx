f838974e41a0596024edebd024204354af897050
hb1998 <33490801+hb1998@users.noreply.github.com>
2021-09-12 01:04:53 +0530

    feat: add basic scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { Component, createSignal, For } from "solid-js";
 import ListRenderer from "./components/ListRenderer";
 
-
 const listData = new Array(1000).fill(null).map((_, index) => ({
   name: `Label-${index}`,
-  height: 50
-}))
-
+  height: 50,
+}));
 
 const App: Component = () => {
-
   return (
-    <ListRenderer height={500} width={500} itemCount={200} itemSize={20} />
-  )
+    <ListRenderer
+      height={500}
+      width={100}
+      itemCount={200}
+      itemSize={20} />
+  );
 };
 
 export default App;
--- src/components/ListRenderer.tsx
+++ src/components/ListRenderer.tsx
@@ -1,61 +1,112 @@
-import { Component, createSignal, For } from "solid-js";
+import { Component, createEffect, createSignal, For, JSX } from "solid-js";
 import styles from "../App.module.css";
 
-
 interface ListRendererProps {
-    height: number;
-    itemCount: number;
-    itemSize: number;
-    width: number;
-    overscanCount?: number;
+  height: number;
+  itemCount: number;
+  itemSize: number;
+  width: number;
+  overscanCount?: number;
+}
+
+interface ItemProps {
+  rowIndex: number;
+  style: JSX.CSSProperties;
 }
 type TWindow = [number, number];
 
 const ListRenderer: Component<ListRendererProps> = ({
-    height,
-    itemCount,
-    itemSize,
-    width,
-    children,
-    overscanCount = 5
+  height,
+  itemCount,
+  itemSize,
+  width,
+  overscanCount = 5,
 }) => {
+  const initialWindowSize = Math.ceil(height / itemSize);
 
-    const initialWindow = Math.ceil(height / itemSize);
+  const [window, setWindow] = createSignal<TWindow>([0, initialWindowSize]);
+  const [scrollState, setScrollState] = createSignal(0);
 
-    const [window, setWindow] = createSignal<TWindow>([0, initialWindow]);
+  const [list, setList] = createSignal([]);
+  let gridContainerRef: HTMLDivElement;
 
-    const [list, setList] = createSignal(getWindowItems(window()));
+  createEffect(() => {
+    const windowItems = getWindowItems({
+      width,
+      window: window(),
+      overscanCount,
+      height,
+      itemCount,
+      itemSize,
+      scrollState: scrollState(),
+    });
+    setList(windowItems);
+  });
 
-    return (
-        <div class={styles.wrapper} >
-            <div style={{
-                "min-height": `${height}px`
-            }} className={styles.container}>
-                <For  each={list()} fallback={<div>Loading...</div>}>
-                    {
-                        (item) =>
-                            <div style={{ height: `${item.height}px` }} >
-                                {item.name}
-                            </div>
-                    }
-                </For>
-            </div>
-        </div>
-    );
+  const handleScroll: JSX.EventHandlerUnion<HTMLDivElement, UIEvent> = (
+    event
+  ) => {
+    event.preventDefault();
+    setScrollState(event.target.scrollTop);
+  };
+
+  return (
+    <div
+      style={{
+        height: wrapPx(height),
+        width: wrapPx(width),
+      }}
+      onscroll={handleScroll}
+      class={styles.wrapper}
+    >
+      <div
+        ref={gridContainerRef}
+        style={{
+          "min-height": wrapPx(itemSize * itemCount),
+        }}
+      >
+        <For each={list()} fallback={<div>Loading...</div>}>
+          {(item) => <div style={item.style}>{item.rowIndex}</div>}
+        </For>
+      </div>
+    </div>
+  );
 };
 
-function getWindowItems([startIdx, endIndex]: [number, number]):number[] {
-    const items = [];
-
-    for (let i = startIdx; i < endIndex; i++) {
-        items.push({
-            rowIndex:i,
-            style:{
-
-            }
-        })
-    }
-    return items;
+function getWindowItems({
+  window,
+  width,
+  height,
+  itemSize,
+  overscanCount,
+  itemCount,
+  scrollState,
+}: {
+  width: number;
+  height: number;
+  itemSize: number;
+  window: [number, number];
+  overscanCount: number;
+  itemCount: number;
+  scrollState: number;
+}) {
+  const items: ItemProps[] = [];
+  const startIndex = Math.max(0, window[0] - overscanCount);
+  const endIndex = Math.min(window[1] + overscanCount, itemCount);
+  for (let index = startIndex; index < endIndex; index++) {
+    items.push({
+      rowIndex: index,
+      style: {
+        height: wrapPx(itemSize),
+        position: "absolute",
+        top: wrapPx((index + 1) * 20),
+      },
+    } as ItemProps);
+  }
+  return items;
 }
 
+function wrapPx(length: number) {
+  return `${length}px`;
+}
 export default ListRenderer;
